b861afbc8c782f4e390ef293139ac8a39ce17712
blared00 <blared009@list.ru>
2021-05-23 21:34:59 +0300

    Добавление коментариев к коду: Админ. панель

--- NNTOS/diary/admin.py
+++ NNTOS/diary/admin.py
@@ -3,10 +3,12 @@ from .models import *
 from import_export.admin import ImportExportActionModelAdmin
 from import_export import resources
 from import_export import  fields
-from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
+from import_export.widgets import ForeignKeyWidget
+"""Визуализация БД  для админ.панели (создание/редактирование объектов БД)"""
 
 
 class StudentAdmin(admin.ModelAdmin):
+    """Таблица студентов"""
     list_display = ('lastname', 'firstname', 'patronymic', 'n_group', )
     list_display_links = ('lastname', )
     search_fields = ('lastname', 'firstname', 'patronymic', )
@@ -15,10 +17,12 @@ class StudentAdmin(admin.ModelAdmin):
 
 
 class Disciplineship(admin.TabularInline):
+    """Форма для отображения дисциплин при создании/редактировании учителя"""
     model = Teacher.discipline.through
 
 
 class TeacherAdmin(admin.ModelAdmin):
+    """Таблица преподавателей"""
     list_display = ('lastname', 'firstname', 'patronymic', )
     list_display_links = ('lastname',  )
     search_fields = ('lastname', 'firstname', 'patronymic', )
@@ -29,30 +33,36 @@ class TeacherAdmin(admin.ModelAdmin):
 
 
 class DisciplineAdmin(admin.ModelAdmin):
+    """Таблица дисциплин"""
     inlines = [Disciplineship, ]
 
 
 class ScheduleGroupInTabular(admin.TabularInline):
+    """Форма отображения пар при создании/редактировании группы"""
     model = ScheduleGroup
     extra = 6
 
 
 class ScheduleGroupResource(resources.ModelResource):
+    """Форма для создания таблиц импорта/экспорта расписания"""
     discipline = fields.Field(column_name='Дисциплина/Преподаватель', attribute='discipline', widget=ForeignKeyWidget(TeacherDiscipline, 'name'))
     n_group = fields.Field(column_name='Номер группы', attribute='n_group', widget=ForeignKeyWidget(StudentGroup, 'number'))
     class_room = fields.Field(column_name='Номер ауд.', attribute='class_room',)
     lesson = fields.Field(column_name='Номер урока', attribute='lesson',widget=ForeignKeyWidget(NumberLesson, 'pk'))
     date = fields.Field(column_name='Дата', attribute='date',)
+
     class Meta:
         model = ScheduleGroup
 
 
 class ScheduleGroupAdmin(ImportExportActionModelAdmin):
+    """Таблица расписания"""
     resource_class = ScheduleGroupResource
     list_display = ['date', 'discipline', 'n_group', 'lesson', 'class_room']
 
 
 class NewsAdmin(admin.ModelAdmin):
+    """Таблица новостей"""
     list_display = ('title', 'published_at', 'published_for_parents', 'published_for_teacher' )
     list_display_links = ('title', )
     search_fields = ('title', )
@@ -61,6 +71,7 @@ class NewsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
+"""Список отображаемых таблиц в админ.панели"""
 admin.site.register(Student, StudentAdmin)
 admin.site.register(StudentGroup,)
 admin.site.register(ScheduleGroup,ScheduleGroupAdmin)
