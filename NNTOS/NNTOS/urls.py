4455241623a641fe92d71388dc54afd16434eafe
blared00 <blared009@list.ru>
2021-05-24 21:50:40 +0300

    Добавление коментариев к коду: Юрл адреса

--- NNTOS/NNTOS/urls.py
+++ NNTOS/NNTOS/urls.py
@@ -1,42 +1,25 @@
-"""NNTOS URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-
-from django.shortcuts import redirect
 from django.urls import path, include
 from diary.views import  ParentsView, TeacherView,CommentView, NewsView, LoginUser, HomeView, logout_view, redirect_page, MarkView
-from NNTOS.settings import MEDIA_URL, MEDIA_ROOT
-
+from NNTOS.settings import MEDIA_URL, MEDIA_ROOT, DEBUG
+"""URL-адреса платформы"""
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('student/<slug:student_name>', ParentsView.as_view(), name='student'),
-    path('teach/<slug:teacher_name>', TeacherView.as_view(), name='teacher'),
-    path('news/<slug:news_slug>', NewsView.as_view(), name='news'),
-    path('comment/<int:comment>', CommentView.as_view(), name='comment'),
-    path('login/', LoginUser.as_view(), name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('redirectpage/', redirect_page, name='redirect_page'),
-    path('formmark/', MarkView.as_view(), name='markview'),
-    # path('formmark/edit/', MarkEditView.as_view(), name='markeditview'),
-    path('grappelli/', include('grappelli.urls')), # grappelli URLS
-    path('', HomeView.as_view(), name='home'),
+    path('admin/', admin.site.urls),                                            #админ.панель
+    path('student/<slug:student_name>', ParentsView.as_view(), name='student'), #страница студента
+    path('teach/<slug:teacher_name>', TeacherView.as_view(), name='teacher'),   #страница преподавателя
+    path('news/<slug:news_slug>', NewsView.as_view(), name='news'),             #детали новости
+    path('comment/<int:comment>', CommentView.as_view(), name='comment'),       #детали комментария
+    path('login/', LoginUser.as_view(), name='login'),                          #страница входа
+    path('logout/', logout_view, name='logout'),                                #страница для работы системы выхода
+    path('redirectpage/', redirect_page, name='redirect_page'),                 #страница для работы системы перенаправления по авторизованному пользователю
+    path('formmark/', MarkView.as_view(), name='markview'),                     #страница проставления оценок
+    path('grappelli/', include('grappelli.urls')), # grappelli URLS             #админ.панель доп функции
+    path('', HomeView.as_view(), name='home'),                                  
+
     # Восстановление пароля
     path('change-password/', auth_views.PasswordResetView.as_view(template_name='step1.html'), name='reset_password'),
     path('change-password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='step2.html'), name='password_reset_done'),
@@ -44,4 +27,6 @@ urlpatterns = [
     path('change-password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='step4.html'), name='password_reset_complete'),
 
 ]
-urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+"""Отображение статических файлов(изображений и т.п.) при открытии на локальном сервере"""
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
